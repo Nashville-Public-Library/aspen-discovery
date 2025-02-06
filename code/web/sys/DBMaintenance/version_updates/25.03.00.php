c786812cb9388d6bf2f3020deb3b5ce6d74968de
Mark Noble <mdnoble73@gmail.com>
2025-02-05 21:55:18 -0700

    Add default files for 25.03

---
+++ code/web/sys/DBMaintenance/version_updates/25.03.00.php
@@ -0,0 +1,34 @@
+<?php
+
+function getUpdates25_03_00(): array {
+	$curTime = time();
+	return [
+		/*'name' => [
+			 'title' => '',
+			 'description' => '',
+			 'continueOnError' => false,
+			 'sql' => [
+				 ''
+			 ]
+		 ], //name*/
+
+		//mark - Grove
+
+		//katherine - Grove
+
+		//kirstien - Grove
+
+		// Leo Stoyanov - BWS
+
+		//alexander - PTFS-Europe
+
+		//chloe - PTFS-Europe
+
+		//James Staub - Nashville Public Library
+
+		//Lucas Montoya - Theke Solutions
+
+		//other
+
+	];
+}
