39d386892b8f56e74f0e357e7b9c6f4e058f70aa
James Staub <james.staub@nashville.gov>
2025-02-12 17:03:49 -0600

    DIS-39: Fix Nashville non-resident fee payment

--- code/web/Drivers/Nashville.php
+++ code/web/Drivers/Nashville.php
@@ -16,6 +16,19 @@ class Nashville extends CarlX {
 		require_once ROOT_DIR . '/sys/SystemVariables.php';
 		$systemVariables = SystemVariables::getSystemVariables();
 
+		// Nashville Non-Resident processing PART 1:
+		// BEFORE recording payments in CarlX,
+		// check if any fines are NON-RESIDENT FEE.
+		// Make an array to cover the case that two or more active non-resident fees are on the account.
+		$carlXFines = $this->getFines($patron);
+		$nonResidentFeeIds = [];
+		foreach ($carlXFines as $fine) {
+			if ($fine['type'] == 'NON-RESIDENT FEE') {
+				// Store nonResidentFee fine id
+				$nonResidentFeeIds[] = $fine['fineId'];
+			}
+		}
+
 		$result = parent::completeFinePayment($patron, $payment);
 		if ($result['success'] === false) {
 			$message = "Online payment CarlX update failed for Payment Reference ID $payment->id. See messages.log for details on individual items.";
@@ -24,18 +37,17 @@ class Nashville extends CarlX {
 			$message = "Online payment successfully recorded in CarlX for Payment Reference ID $payment->id.";
 			$level = Logger::LOG_DEBUG;
 
-			// Nashville Non-Resident processing:
+			// Nashville Non-Resident processing PART 2:
+			// AFTER all payments succeed,
 			// Look for Non-Resident Fee associated with patron's account and whether it is included in this payment
 			// If so, update the patron's expiration date
-			$accountLinesPaid = explode(',', $payment->finesPaid);
-			$allFines = $this->getFines($patron);
-			foreach ($accountLinesPaid as $line) {
-				[$feeId, $pmtAmount] = explode('|', $line);
-				foreach ($allFines as $fine) {
-					if ($fine['fineId'] == $feeId) {
-						if ($fine['type'] == 'NON-RESIDENT FEE') {
+			if (!empty($nonResidentFeeIds)) {
+				$accountLinesPaid = explode(',', $payment->finesPaid);
+				foreach ($accountLinesPaid as $line) {
+					[$feeId, $pmtAmount] = explode('|', $line);
+					foreach ($nonResidentFeeIds as $nonResidentFeeId) {
+						if ($nonResidentFeeId == $feeId) {
 							$this->updateNonResident($patron);
-							break;
 						}
 					}
 				}
