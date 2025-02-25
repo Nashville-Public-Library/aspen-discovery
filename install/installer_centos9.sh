6ea7f60aa65edaf9f9dc1dc056c14f9ff2557fae
James Staub <james.staub@nashville.gov>
2025-02-25 17:15:57 -0600

    DIS-434: replace yum with dnf in installer_centos9.sh

--- install/installer_centos9.sh
+++ install/installer_centos9.sh
@@ -5,22 +5,22 @@ git config --global --add safe.directory /usr/local/aspen-discovery
 #This will need to be copied to the server manually to do the setup.
 #Expects to be installed on CentOS9
 #Run as sudo ./installer_centos9.sh
-yum check-update
-yum -y install wget
-yum -y install curl
-yum -y install httpd
-yum -y install http://rpms.remirepo.net/enterprise/remi-release-9.rpm
-yum -y install yum-utils
-yum-config-manager --enable remi-php83
-yum -y install php php-mcrypt php-gd php-curl php-mysql php-zip php-fileinfo php-soap
-yum -y install php-xml
-yum -y install bind-utils
-yum -y install php-intl
-yum -y install php-mbstring
-yum -y install php-pecl-ssh2
-yum -y install php-pgsql
-yum -y install php-imagick
-yum -y install php-ldap
+dnf check-update
+dnf -y install wget
+dnf -y install curl
+dnf -y install httpd
+dnf -y install http://rpms.remirepo.net/enterprise/remi-release-9.rpm
+dnf -y install dnf-utils
+dnf config-manager --enable remi-php83
+dnf -y install php php-mcrypt php-gd php-curl php-mysql php-zip php-fileinfo php-soap
+dnf -y install php-xml
+dnf -y install bind-utils
+dnf -y install php-intl
+dnf -y install php-mbstring
+dnf -y install php-pecl-ssh2
+dnf -y install php-pgsql
+dnf -y install php-imagick
+dnf -y install php-ldap
 service httpd start
 chkconfig httpd on
 # New PHP ini file
@@ -40,11 +40,11 @@ mv /etc/my.cnf /etc/my.cnf.old
 cp my.cnf /etc/my.cnf
 systemctl start mariadb
 systemctl enable mariadb
-yum -y install java-17-openjdk
-yum -y install unzip
-yum -y install strace
-yum -y install mytop
-yum -y install mysqltuner
+dnf -y install java-17-openjdk
+dnf -y install unzip
+dnf -y install strace
+dnf -y install mytop
+dnf -y install mysqltuner
 
 #Create temp smarty directories
 cd /usr/local/aspen-discovery
@@ -52,20 +52,16 @@ mkdir tmp
 chown -R apache:apache tmp
 chmod -R 755 tmp
 
-#Disable SELinux
-setenforce 0
-cp install/selinux.config /etc/selinux/config
-
 #Increase entropy
-yum -y -q install rng-tools
+dnf -y -q install rng-tools
 cp install/limits.conf /etc/security/limits.conf
 cp install/rngd.service /etc/systemd/system/multi-user.target.wants/rngd.service
 
 systemctl daemon-reload
 systemctl start rngd
 
-yum -y install epel-release
-yum -y install certbot python2-certbot-apache
+dnf -y install epel-release
+dnf -y install certbot python2-certbot-apache
 
 echo "Generate new root password for mariadb at: https://passwordsgenerator.net/ and store in passbolt"
 mariadb-secure-installation
@@ -85,12 +81,12 @@ echo -e "ServerSignature Off \nServerTokens Prod" >> /etc/httpd/conf/httpd.conf
 
 # mod evasive is causing issues with sites that have lots of book covers on one page. Not installing.
 # configure mod evasive
-#yum install mod_evasive -y
+#dnf install mod_evasive -y
 #cp mod_evasive.conf /etc/httpd/conf.d/mod_evasive.conf
 #mdir /var/log/mod_evasive
-yum remove mod_evasive -y
+dnf remove mod_evasive -y
 
 # mod security is causing issues with file uploads.  Not installing.
 #configure mod security
-#yum install mod_security -y
-yum remove mod_security -y
+#dnf install mod_security -y
+dnf remove mod_security -y
