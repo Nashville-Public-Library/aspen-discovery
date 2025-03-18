02ed2707aac1abad69575f8f64abb4159db036d5
Katherine Perdue <Abhyastamita@gmail.com>
2025-03-17 21:59:08 -0400

    DIS-192: Increase length of event field value and event field value default to TEXT
    - Length was 100 characters which doesn't make sense for text areas

--- code/web/sys/Events/EventFieldSet.php
+++ code/web/sys/Events/EventFieldSet.php
@@ -169,7 +169,6 @@ class EventFieldSet extends DataObject {
 					'description' => $field->description,
 					'default' => $field->defaultValue,
 					'facetName' => $field->facetName,
-					'maxLength' => 100,
 				];
 				if ($type == 'enum') {
 					$structure[$field->id]['values'] = explode("\n", $field->allowableValues);
