eb19296e48cf13add9b7d625f123615ea430896c
LeoStoyanovByWater <leo.stoyanov@bywatersolutions.com>
2025-04-10 13:52:03 -0500

    DIS-396: Added DB update and release notes.

--- code/web/sys/DBMaintenance/version_updates/25.05.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.05.00.php
@@ -23,6 +23,15 @@ function getUpdates25_05_00(): array {
 		//Yanjun Li - ByWater
 
 		// Leo Stoyanov - BWS
+		'ip_lookup_ipv6_support' => [
+			'title' => 'Add Support for IPv6 Addresses',
+			'description' => 'Add support for IPv6 addresses in ip_lookup table.',
+			'continueOnError' => true,
+			'sql' => [
+				"ALTER TABLE ip_lookup MODIFY startIpVal VARCHAR(255) NULL COMMENT 'Numeric value for IPv4 or encoded string for IPv6'",
+				"ALTER TABLE ip_lookup MODIFY endIpVal VARCHAR(255) NULL COMMENT 'Numeric value for IPv4 or encoded string for IPv6'"
+			],
+		], //ip_lookup_ipv6_support
 
 		//alexander - PTFS-Europe
 
