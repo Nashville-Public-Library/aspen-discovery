15f788b1f0128d9715248cde75552191be21a13a
LeoStoyanovByWater <leo.stoyanov@bywatersolutions.com>
2025-04-18 13:31:20 -0500

    DIS-671: Increased the size of the custom form field list values and improved textarea UI.

--- code/web/sys/DBMaintenance/version_updates/25.05.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.05.00.php
@@ -31,6 +31,13 @@ function getUpdates25_05_00(): array {
 		//Yanjun Li - ByWater
 
 		// Leo Stoyanov - BWS
+		'custom_form_field_enums_to_text' => [
+			'title' => 'Increase Custom Form Field EnumValues Size',
+			'description' => 'Changes the enumValues column in web_builder_custom_form_field from VARCHAR(255) to TEXT to allow for longer select lists.',
+			'sql' => [
+				"ALTER TABLE web_builder_custom_form_field MODIFY COLUMN enumValues TEXT DEFAULT NULL",
+			]
+		],
 
 		//alexander - PTFS-Europe
 
