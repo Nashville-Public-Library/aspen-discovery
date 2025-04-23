a870b92be8ed11810bc78fe93635bb3b37be65ae
LeoStoyanovByWater <leo.stoyanov@bywatersolutions.com>
2025-04-23 18:10:29 -0500

    DIS-699: Allow placard images max-height to be configurable in Themes.

--- code/web/sys/DBMaintenance/version_updates/25.05.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.05.00.php
@@ -31,6 +31,13 @@ function getUpdates25_05_00(): array {
 		//Yanjun Li - ByWater
 
 		// Leo Stoyanov - BWS
+		'add_placard_image_max_height_to_themes' => [
+			'title' => 'Add Placard Image Max Height to Themes',
+			'description' => 'Adds a placardImageMaxHeight column to the themes table to control placard image height.',
+			'sql' => [
+				"ALTER TABLE themes ADD COLUMN IF NOT EXISTS `placardImageMaxHeight` INT DEFAULT 0",
+			]
+		], //add_placard_image_max_height_to_themes
 
 		//alexander - PTFS-Europe
 
