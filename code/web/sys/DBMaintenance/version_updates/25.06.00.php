5ec73bee131511d3b21b066afa8ed164a4170c77
Mark Noble <mdnoble73@gmail.com>
2025-05-01 07:58:31 -0600

    Add default release notes and version updates

---
+++ code/web/sys/DBMaintenance/version_updates/25.06.00.php
@@ -0,0 +1,40 @@
+<?php
+
+function getUpdates25_06_00(): array {
+	$curTime = time();
+	return [
+		/*'name' => [
+			 'title' => '',
+			 'description' => '',
+			 'continueOnError' => false,
+			 'sql' => [
+				 ''
+			 ]
+		 ], //name*/
+
+		//mark - Grove
+
+		//katherine - Grove
+
+		//kirstien - Grove
+
+		//kodi - Grove
+
+		//Yanjun Li - ByWater
+
+		// Leo Stoyanov - BWS
+
+		// Laura Escamilla - ByWater Solutions
+
+		//alexander - Open Fifth
+
+		//chloe - Open Fifth
+
+		//James Staub - Nashville Public Library
+
+		//Lucas Montoya - Theke Solutions
+
+		//other
+
+	];
+}
