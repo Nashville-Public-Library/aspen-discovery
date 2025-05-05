e0e2014c881ec50c5432159fb97cfd37519c0453
Lucas Montoya <lucasmontoya@theke.io>
2025-05-05 08:22:01 -0300

    DIS-599: Check if patron'account is debarred.
    - Add a method to get the status of the user account, especifically if it is debarred.
    - Include the base method in the parent classes

--- code/web/Drivers/Koha.php
+++ code/web/Drivers/Koha.php
@@ -5868,6 +5868,26 @@ class Koha extends AbstractIlsDriver {
 	}
 
 	/**
+	 * Check if the patron is debarred (frozen).
+	 *
+	 * The debarment status could be useful to allow or disallow the user to perform certain actions.
+	 * @param User $patron
+	 * @return bool $debarmentStatus
+	 */
+	public function getDebarmentStatus(User $patron) {
+		$debarmentStatus = false;
+		$borrowerNumber = $patron->unique_ils_id;
+		$endpoint = "/api/v1/patrons/$borrowerNumber";
+		$response = $this->kohaApiUserAgent->get($endpoint,'koha.getDebarmentStatus',[],[]);
+		if($response){
+			if($response['code'] == 200){
+				$debarmentStatus = $response['content']['restricted'];
+			}
+		}
+		return $debarmentStatus;
+	}
+
+	/**
 	 * @param array $postFields
 	 * @param string $postFieldName
 	 * @param string $requestFieldName
@@ -6591,6 +6611,19 @@ class Koha extends AbstractIlsDriver {
 				'isPublicFacing' => true,
 			]);
 		}
+
+		//Check if the patron is frozen
+		if($this->getDebarmentStatus($patron)){
+			$result['isEligible'] = false;
+			if (strlen($result['message']) > 0) {
+				$result['message'] .= '<br/>';
+			}
+			$result['message'] .= translate([
+				'text' => 'Sorry, your account is debarred and you are not able to place holds.',
+				'isPublicFacing' => true,
+			]);
+		}
+
 
 		//Check if the patron is expired
 		if ($accountSummary->isExpired()) {
