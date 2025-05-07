1abfc3a7df49ca18de2d42257259e4e1bffddddd
Katherine Perdue <Abhyastamita@gmail.com>
2025-05-07 11:20:34 -0400

    DIS-780: Add barcode entry keyboard type to LiDA self check settings and the API

--- code/web/sys/DBMaintenance/version_updates/25.06.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.06.00.php
@@ -15,6 +15,13 @@ function getUpdates25_06_00(): array {
 		//mark - Grove
 
 		//katherine - Grove
+		'add_lida_barcode_entry_keyboard_type_setting' => [
+			'title' => 'Add a barcode entry keyboard type field to LiDA self check settings',
+			'description' => 'Add keyboard type options to use numeric keyboard, alphanumeric keyboard, or not to allow keyboard entry',
+			'sql' => [
+				"ALTER TABLE aspen_lida_self_check_settings ADD COLUMN barcodeEntryKeyboardType TINYINT(1) NOT NULL DEFAULT 1;",
+			]
+		], //add_lida_barcode_entry_keyboard_type_setting
 
 		//kirstien - Grove
 
