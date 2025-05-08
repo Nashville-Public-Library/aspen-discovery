a667c6c6da480b6f88ddbab179fe3c8b43b81667
LeoStoyanovByWater <leo.stoyanov@bywatersolutions.com>
2025-05-08 09:47:11 -0500

    DIS-766: DB maintenance and release notes.

--- code/web/sys/DBMaintenance/version_updates/25.06.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.06.00.php
@@ -23,6 +23,17 @@ function getUpdates25_06_00(): array {
 		//Yanjun Li - ByWater
 
 		// Leo Stoyanov - BWS
+		'reading_history_columns_and_index' => [
+			'title' => 'Add Force Reading History Load Flag, Reading History Import Start Datetime, & Index',
+			'description' => 'Add a flag to force immediate loading of reading history for users, a reading history import start datetime, and an index of initial reading history loaded and the previous two new columns.',
+			'continueOnError' => false,
+			'sql' => [
+				"ALTER TABLE user ADD COLUMN IF NOT EXISTS forceReadingHistoryLoad TINYINT(1) DEFAULT 0",
+				"ALTER TABLE user ADD COLUMN IF NOT EXISTS readingHistoryImportStartedAt DATETIME DEFAULT NULL",
+				"DROP INDEX IF EXISTS idx_reading_history_import_status ON user",
+				"CREATE INDEX idx_reading_history_import_status ON user (initialReadingHistoryLoaded, forceReadingHistoryLoad, readingHistoryImportStartedAt)"
+			]
+		], //reading_history_columns_and_index
 
 		// Laura Escamilla - ByWater Solutions
 
