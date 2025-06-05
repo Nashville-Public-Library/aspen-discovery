5bd09d353713e65d5b62433c5a8bc3030b00dd1f
Mark Noble <mdnoble73@gmail.com>
2025-06-04 20:24:55 -0600

    Add default readme and database update files

---
+++ code/web/sys/DBMaintenance/version_updates/25.07.00.php
@@ -0,0 +1,44 @@
+<?php
+
+function getUpdates25_07_00(): array {
+	$curTime = time();
+	return [
+		/*'name' => [
+			 'title' => '',
+			 'description' => '',
+			 'continueOnError' => false,
+			 'sql' => [
+				 ''
+			 ]
+		 ], //name*/
+
+		//mark - Grove
+
+		//katherine - Grove
+
+		//kirstien - Grove
+
+		//kodi - Grove
+
+		//Mark - Grove
+
+		// Myranda - Grove
+
+		//Yanjun Li - ByWater
+
+		// Leo Stoyanov - BWS
+
+		// Laura Escamilla - ByWater Solutions
+
+		//alexander - Open Fifth
+
+		//chloe - Open Fifth
+
+		//James Staub - Nashville Public Library
+
+		//Lucas Montoya - Theke Solutions
+
+		//other
+
+	];
+}
