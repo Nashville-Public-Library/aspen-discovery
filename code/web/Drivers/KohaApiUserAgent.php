8836812806a72a2cfadc5a1c2ef94d63c7809fb1
Lucas Montoya <lucasmontoya@theke.io>
2025-06-10 16:39:28 -0300

    DIS-951: Improve documentation about the 'delete' method

--- code/web/Drivers/KohaApiUserAgent.php
+++ code/web/Drivers/KohaApiUserAgent.php
@@ -258,21 +258,28 @@ class KohaApiUserAgent {
 	}
 
 	/**
-	 * Makes an API call via DELETE method
-	 *
-	 * Makes an API call to Koha using DELETE method and get a response body and a response code on success.
-	 * Recover specific data of it as properties.
-	 *
-	 * @param string $endpoint e.g "/api/v1/holds/$itemToThawId/suspension"
-	 * @param string $caller e.g "koha.thawHold"
-	 * @param array $dataToSanitize e.g []
-	 * @param array|null $extraHeaders e.g ['Accept-Encoding: gzip, deflate']
-	 *
-	 * @return  array|bool     An array containing the response body and response code retrieved by the request or false if authorization fails.
-	 * @access  public
-	 */
-	public function delete(string $endpoint, string $caller, array $dataToSanitize = [], array $extraHeaders = null): array|bool {
-		//Preparing request
+	* Performs an API call using the DELETE method.
+	*
+	* Sends a DELETE request to the specified endpoint of Koha,
+	* and returns an array with :
+	* - content
+	* - status code
+	* - url
+	* - headers
+	*
+	* on success.
+	* @param string			$endpoint				The API endpoint to call (e.g. "/api/v1/password/validation").
+	* @param string			$caller					Identifier of the calling service (e.g. "koha.patronlogin").
+	* @param array			$dataToSanitize			Associative array of data to sanitize (e.g. ['password' => $password]).
+	* @param ?array			$extraHeaders			Optional headers to include in the request (e.g. ['Example: Example']).
+	*
+	* @return array{content: mixed, code: int, url: string, headers: array[string]}|false Returns an associative array with the response body and status code, or false on authorization failure.
+	*
+	* @access public
+	*/
+	public function delete(string $endpoint, string $caller, array $dataToSanitize = [], ?array $extraHeaders = null): array|bool {
+
+		// Prepare the request
 		$apiURL = $this->baseURL . $endpoint;
 
 		if ($this->getAuthorizationHeader($caller)) {
@@ -283,14 +290,20 @@ class KohaApiUserAgent {
 			return false;
 		}
 
+		// Add default headers
 		$this->apiCurlWrapper->addCustomHeaders($this->defaultHeaders, false);
+
+		// Add custom headers
 		if (isset($extraHeaders)) {
 			$this->apiCurlWrapper->addCustomHeaders($extraHeaders, false);
 		}
-		//Getting response body
+
+		//Get the response body
 		$response = $this->apiCurlWrapper->curlSendPage($apiURL, 'DELETE', '');
 		$responseCode = $this->apiCurlWrapper->getResponseCode();
 		$jsonResponse = $this->jsonValidate($response);
+
+		// Catch and save information about the API request
 		ExternalRequestLogEntry::logRequest($caller, 'DELETE', $apiURL, $this->apiCurlWrapper->getHeaders(), '', $responseCode, $response, $dataToSanitize);
 		return [
 			'content' => $jsonResponse,
