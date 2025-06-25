8b9a9ae235ad11c4d75b49f924c88c74cf8ede50
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2025-06-25 15:14:44 +0000

    DIS-1019: Database changes

--- code/web/sys/DBMaintenance/version_updates/25.08.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.08.00.php
@@ -35,6 +35,16 @@ function getUpdates25_08_00(): array {
 
 		//Jacob - Open Fifth
 
+		//Pedro - Open Fifth
+		'add_timestamp_to_ce_campaign_milestone_progress_entries' => [
+			'title' => 'Update ce_campaign_milestone_progress_entries',
+			'description' => 'Add timestamp to ce_campaign_milestone_progress_entries',
+			'sql' => [
+				"ALTER TABLE ce_campaign_milestone_progress_entries ADD COLUMN `timestamp` datetime NOT NULL DEFAULT (CURRENT_TIME)",
+			],
+		], //add_timestamp_to_ce_campaign_milestone_progress_entries
+
+
 		//James Staub - Nashville Public Library
 
 		//Lucas Montoya - Theke Solutions
