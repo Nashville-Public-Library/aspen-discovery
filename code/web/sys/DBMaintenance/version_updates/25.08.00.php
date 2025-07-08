e3d2241963d7eaf4633a7436d1b02b446b7f50d6
LeoStoyanovByWater <leo.stoyanov@bywatersolutions.com>
2025-07-08 17:36:22 -0500

    DIS-1057: DB update to add maxHoldCancellationDate.

--- code/web/sys/DBMaintenance/version_updates/25.08.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.08.00.php
@@ -25,6 +25,14 @@ function getUpdates25_08_00(): array {
 		//Yanjun Li - ByWater
 
 		// Leo Stoyanov - BWS
+		'add_max_hold_cancellation_date_field' => [
+			'title' => 'Add Max Hold Cancellation Date Field',
+			'description' => 'Add the Max Hold Cancellation Date field for when hold cancellations are enabled.',
+			'continueOnError' => true,
+			'sql' => [
+				'ALTER TABLE library ADD COLUMN IF NOT EXISTS maxHoldCancellationDate int(11) DEFAULT -1 AFTER defaultNotNeededAfterDays;'
+			]
+		], //add_max_hold_cancellation_date_field
 
 		// Laura Escamilla - ByWater Solutions
 
