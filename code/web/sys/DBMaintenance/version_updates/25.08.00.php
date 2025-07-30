19bb2ae7a3650833034a5db3fd3b428d0c3e890e
Kodi <kodilein@gmail.com>
2025-07-30 14:34:08 -0700

    DIS-995: While You Wait and You Might Also Like Improvements
    - Add scoping options for While You Wait and You Might Also Like to limit by owning library and format
    - Add new parameters to the getWhileYouWait and getYouMightAlsoLike functions to pass format and search source
    - Add /GroupedWork/*/Home?* to robots.txt to prevent crawling pages with the new activeFormat and activeSearchSource URL parameters
    - Update release notes
    
    Tested on my local instance of Aspen

--- code/web/sys/DBMaintenance/version_updates/25.08.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.08.00.php
@@ -19,7 +19,14 @@ function getUpdates25_08_00(): array {
 		//kirstien - Grove
 
 		//kodi - Grove
-
+		'library_you_might_also_like' => [
+			'title' => 'Library You Might Also Like Setting',
+			'description' => 'Add a setting for libraries for the "You Might Also Like" feature to disable or enable with restrictions.',
+			'sql' => [
+				"ALTER TABLE library ADD COLUMN showYouMightAlsoLike TINYINT(1) DEFAULT 1;",
+				"UPDATE library SET showYouMightAlsoLike =0 WHERE showWhileYouWait=0;"
+			],
+		], //library_you_might_also_like
 		// Myranda - Grove
 
 		//Yanjun Li - ByWater
