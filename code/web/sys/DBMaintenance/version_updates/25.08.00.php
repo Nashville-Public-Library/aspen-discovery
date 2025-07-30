604753ae6f3dc89bb708d1ffe77de2c1fe377a2e
LeoStoyanovByWater <leo.stoyanov@bywatersolutions.com>
2025-07-30 15:46:38 -0500

    DIS-1170: Change RecordToInclude database column defaults to match the PHP defaults for `includeHoldableOnly`, `includeItemsOnOrder`, and `includeEContent`.

--- code/web/sys/DBMaintenance/version_updates/25.08.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.08.00.php
@@ -25,6 +25,19 @@ function getUpdates25_08_00(): array {
 		//Yanjun Li - ByWater
 
 		// Leo Stoyanov - BWS
+		'update_record_to_include_defaults' => [
+			'title' => 'Update RecordToInclude Column Defaults to Match PHP Defaults',
+			'description' => 'Update database column defaults for includeHoldableOnly, includeItemsOnOrder, and includeEContent to match the PHP class defaults.',
+			'continueOnError' => false,
+			'sql' => [
+				'ALTER TABLE library_records_to_include CHANGE COLUMN includeHoldableOnly includeHoldableOnly tinyint(1) NOT NULL DEFAULT 0',
+				'ALTER TABLE library_records_to_include CHANGE COLUMN includeItemsOnOrder includeItemsOnOrder tinyint(1) NOT NULL DEFAULT 1',
+				'ALTER TABLE library_records_to_include CHANGE COLUMN includeEContent includeEContent tinyint(1) NOT NULL DEFAULT 1',
+				'ALTER TABLE location_records_to_include CHANGE COLUMN includeHoldableOnly includeHoldableOnly tinyint(1) NOT NULL DEFAULT 0',
+				'ALTER TABLE location_records_to_include CHANGE COLUMN includeItemsOnOrder includeItemsOnOrder tinyint(1) NOT NULL DEFAULT 1',
+				'ALTER TABLE location_records_to_include CHANGE COLUMN includeEContent includeEContent tinyint(1) NOT NULL DEFAULT 1',
+			]
+		], //update_record_to_include_defaults
 
 		// Laura Escamilla - ByWater Solutions
 
