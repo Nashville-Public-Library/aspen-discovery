01dae55b33e38fe15ca6c6a3a7090235979b81a8
Katherine Perdue <Abhyastamita@gmail.com>
2025-08-03 15:43:05 -0400

    DIS-884: Add token for messaging settings URL to welcome email templace

--- code/web/sys/Email/EmailTemplate.php
+++ code/web/sys/Email/EmailTemplate.php
@@ -298,6 +298,7 @@ class EmailTemplate extends DataObject {
 
 		$text = str_replace('%library.displayName%', $library->displayName ?? '', $text);
 		$text = str_replace('%library.baseUrl%', $baseUrl, $text);
+		$text = str_ireplace('%library.messagingSettingsUrl%', $baseUrl . "/MyAccount/MessagingSettings", $text);
 		$text = str_replace('%library.email%', $library->contactEmail ?? '', $text);
 		$text = str_ireplace('%user.firstname%', $user->firstname ?? '', $text);
 		$text = str_ireplace('%user.lastname%', $user->lastname ?? '', $text);
