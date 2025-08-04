8dc35e788753a430defb4302a46d36f953bcdb08
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2025-08-04 14:38:46 +0100

    DIS-1186: feat: update db to increase max length
    Increase the max length of the display name in locations

--- code/web/sys/DBMaintenance/version_updates/25.09.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.09.00.php
@@ -47,6 +47,14 @@ function getUpdates25_09_00(): array {
 		// Leo Stoyanov - BWS
 
 		//alexander - Open Fifth
+		'increase_location_display_name_allowed_length' => [
+			'title' => 'Increase Location Display Name Allowed Length',
+			'description' => 'Increase the allowed length for the location display name',
+			'continueOnError' => false,
+			'sql' => [
+				'ALTER TABLE location MODIFY displayName VARCHAR(100) NOT NULL'
+			],
+		], // increase_location_display_name_allowed_length
 
 		//chloe - Open Fifth
 
